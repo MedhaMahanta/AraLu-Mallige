d32f17087eb8bf9ac9af1085b43eb3ea9c8c2228
Medha M <79490974+MedhaMahanta@users.noreply.github.com>
2021-11-06 18:45:28 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-window.addEventListener('mouseup', function(event) {
-	var sidePanel = document.getElementById("mySidepanel");
-	if ((event.target != sidePanel) && (event.target.parentNode != sidePanel)) {
-		sidePanel.style.width = "0";
-	}
-});
+// window.addEventListener('mouseup', function(event) {
+// 	var sidePanel = document.getElementById("mySidepanel");
+// 	if ((event.target != sidePanel) && (event.target.parentNode != sidePanel)) {
+// 		sidePanel.style.width = "0";
+// 	}
+// });
 
 /* For home page*/
 var slideIndex = 0;
@@ -32,4 +32,20 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3000); 
+}
+
+function openColumn(open) {
+  for (i = 1; i < 6; i++) {
+    nameOfClosedTab = "tab" + i.toString();
+    nameOfClosedPage = "page" + i.toString();
+    document.getElementById(nameOfClosedPage).style.display = "none";
+    if (document.getElementById(nameOfClosedTab).classList.contains("release-categories-clicked")) {
+      document.getElementById(nameOfClosedTab).classList.remove("release-categories-clicked");
+    }
+  }
+  nameOfOpenedPage = "page" + open.toString();
+  nameOfOpenedTab = "tab" + open.toString();
+  document.getElementById(nameOfOpenedPage).style.display = "block";
+  document.getElementById(nameOfOpenedTab).classList.add("release-categories-clicked");
+
 }
